332eebd89f1a4fe5efc4d158f9fe961e7620a1da
Dipzap <truongcao1207@gmail.com>
2022-08-22 21:14:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+
+import "swiper/scss";
+
+import HomePage from "./pages/HomePage";
+import Movies from "./pages/Movies";
+import Header from "./layout/Header";
+import MoviesDetails from "./pages/MoviesDetails";
+import TvShows from "./pages/TvShows";
+import TvShowsDetail from "./pages/TvShowDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header></Header>
+              <HomePage></HomePage>
+            </>
+          }
+        ></Route>
+
+        <Route
+          path="/movies"
+          element={
+            <>
+              <Header></Header>
+              <Movies></Movies>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/tvshows"
+          element={
+            <>
+              <Header></Header>
+              <TvShows></TvShows>
+            </>
+          }
+        ></Route>
+
+        <Route
+          path="/movies/:movieId"
+          element={
+            <>
+              <Header></Header>
+              <MoviesDetails></MoviesDetails>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/tv/:movieId"
+          element={
+            <>
+              <Header></Header>
+              <TvShowsDetail></TvShowsDetail>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </>
   );
 }
 
